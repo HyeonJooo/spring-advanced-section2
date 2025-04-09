0057c82e6bf3133a5dda26a22fc2e74e71fc802a
HyeonJooo <parkhj2416@gmail.com>
2025-04-09 14:50:36 +0900

    [섹션3] 쓰레드 로컬 동기화 - 적용

--- src/main/java/hello/advanced/LogTraceConfig.java
+++ src/main/java/hello/advanced/LogTraceConfig.java
@@ -3,14 +3,14 @@ package hello.advanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import hello.advanced.trace.logtrace.FiledLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
 	@Bean
 	public LogTrace logTrace() {
-		return new FiledLogTrace();
+		return new ThreadLocalLogTrace();
 	}
 }
